e4bbb3432893810d041720577190782008f97dd3
odennis25 <odennis25@student.gvsd.org>
2023-04-30 11:49:22 -0400

    added some packages to help organization
    Signed-off-by: odennis25 <odennis25@student.gvsd.org>

--- src/core/RTSMain.java
+++ src/core/RTSMain.java
@@ -1,5 +1,6 @@
+package core;
 
-public class GameMain {
+public class RTSMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
---
+++ src/map/MapRenderer.java
@@ -0,0 +1,9 @@
+package map;
+
+public class MapRenderer {
+
+	public MapRenderer() {
+		
+	}
+	
+}
