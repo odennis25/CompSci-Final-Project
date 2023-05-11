9ccfcee1fa01f553604f80f4cb3f3f55dfc9cb3f
odennis25 <odennis25@student.gvsd.org>
2023-05-11 18:48:36 -0400

    TF IS HEAD>??????

--- src/core/RTSMain.java
+++ src/core/RTSMain.java
@@ -37,13 +37,12 @@ public class RTSMain extends GameApplication
 	private Entity[][] unitEntitys= new Entity[21][21];
 	private ArrayList<Entity> terrainEntitys= new ArrayList<Entity>();
 	private Camera camera;
-<<<<<<< HEAD
-=======
+
 	private int mouseX;
 	private int mouseY;
 	
 	
->>>>>>> 836dc12011c80a38215617899b398f5015439eba
+
 
 	@Override
 	/**Initializes settings*/
